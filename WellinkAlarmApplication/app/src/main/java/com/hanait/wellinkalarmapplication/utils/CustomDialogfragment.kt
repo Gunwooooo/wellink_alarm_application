821f022a67be2df045718fd08b4420e78fcbc1a3
DESKTOP-9V3VU36\USER <hd7107@hana-it.com>
2022-05-10 17:33:42 +0900

    캘린더 다이어로그 작성 및 item view에 들어갈 컨텐츠 임의 제작

---
+++ WellinkAlarmApplication/app/src/main/java/com/hanait/wellinkalarmapplication/utils/CustomDialogfragment.kt
@@ -0,0 +1,37 @@
+package com.hanait.wellinkalarmapplication.utils
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.hanait.wellinkalarmapplication.R
+import android.content.DialogInterface
+import android.graphics.Rect
+import android.view.*
+import android.view.LayoutInflater
+
+
+
+
+class DialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+        val inflater = requireActivity().layoutInflater
+        val view: View = inflater.inflate(R.layout.home_calendar_dialog, null)
+        builder.setView(view)
+        return builder.create()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.home_calendar_dialog, container,false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        return view
+    }
+}
